9a8212b5170dfd3224474ae89b2fdba024fd12db
gretakay <monica24685@gmail.com>
2026-07-11 16:44:25 +0800

    feat: update API_BASE_URL in configuration for Google Apps Script

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
   // 貼上你部署好的 Google Apps Script Web App URL
   // 例如: https://script.google.com/macros/s/AKfycb.../exec
-  API_BASE_URL: ""
+  API_BASE_URL: "https://script.google.com/macros/s/AKfycbwkIDDc3tSXTbtLnbWCazO4_o4jxaOFHqgbuHhvDnlJiRHkPsvOyfN1WgnYfcTT66PT/exec"
 };
